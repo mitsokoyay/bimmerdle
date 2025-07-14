c3b34ad5b3f5d6538534758df937087998d3d623
Dylan Berutti <beruttid@gmail.com>
2025-07-14 00:04:05 -0400

    added more answer boxes

--- pmo.js
+++ pmo.js
@@ -1,11 +1,23 @@
 function checkGuess() {
     var firstInput = document.getElementById("guess-input");
     var result = document.getElementById("result-text");
-    var modelExInput = document.getElementById("model-input");
-    var modelInput = modelExInput.value.trim().toLowerCase();
+    var modelInput = document.getElementById("model-input");
+    var modelCheckInput = modelInput.value.trim().toLowerCase();
+    var modelResult = document.getElementById("model-result");
+    var chassisInput = document.getElementById("chassis-code");
+    var chassisCheckInput = chassisInput.value.trim().toLowerCase();
+    var chassisResult = document.getElementById("chassis-result");
     var car = { model: "3 Series", chassisCode: "E46", make: "BMW", };
-    if (modelInput == car.model.toLowerCase()) {
-        result.textContent = "Correct!";
+    if (modelCheckInput == car.model.toLowerCase()) {
+        modelResult.textContent = "Correct!";
+        modelResult.style.color = "green";
+    }
+    else {
+        modelResult.textContent = "Incorrect";
+        modelResult.style.color = "red";
+    }
+    if (chassisCheckInput == car.chassisCode.toLowerCase()) {
+        chassisResult.textContent = "Correct!";
         result.style.color = "green";
     }
     else {
--- pmo.ts
+++ pmo.ts
@@ -1,17 +1,31 @@
    function checkGuess(): void{
         const firstInput = document.getElementById("guess-input") as HTMLInputElement;
         const result = document.getElementById("result-text") as HTMLParagraphElement;
-        const modelExInput = document.getElementById("model-input") as HTMLInputElement;
-        const modelInput = modelExInput.value.trim().toLowerCase();
+        const modelInput = document.getElementById("model-input") as HTMLInputElement;
+        const modelCheckInput = modelInput.value.trim().toLowerCase();
+        const modelResult = document.getElementById("model-result") as HTMLParagraphElement;
+        const chassisInput = document.getElementById("chassis-code") as HTMLInputElement;
+        const chassisCheckInput = chassisInput.value.trim().toLowerCase();
+        const chassisResult = document.getElementById("chassis-result") as HTMLParagraphElement;
         const car = {model: "3 Series", chassisCode: "E46", make: "BMW", }
 
-        if(modelInput == car.model.toLowerCase()){
-          result.textContent = "Correct!";
+        if(modelCheckInput == car.model.toLowerCase()){
+          modelResult.textContent = "Correct!";
+          modelResult.style.color = "green";
+        }
+        else{
+          modelResult.textContent = "Incorrect";
+          modelResult.style.color= "red";
+        }
+
+        if(chassisCheckInput == car.chassisCode.toLowerCase()){
+          chassisResult.textContent = "Correct!";
           result.style.color = "green";
+
         }
         else{
           result.textContent = "Incorrect";
-          result.style.color= "red";
+          result.style.color = "red";
         }
         
       }
