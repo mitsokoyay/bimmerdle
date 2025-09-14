43558acc4bf3b46d22c7aa58c398c14fd80d376f
Dylan Berutti <beruttid@gmail.com>
2025-09-14 01:55:47 -0400

    fixing up the code and refining it a lot

--- pmo.ts
+++ pmo.ts
@@ -5,94 +5,83 @@ interface Car {
   year: string;
   image: string;
 }
+const renderedCars: Car[] = [
+  {
+    id: "BMW 3 Series",
+    name: "3 Series",
+    chassis: "E46",
+    year: "2004",
+    image: "bmwimages/3series/1.jpg",
+  },
+  {
+    id: "BMW 3 Series",
+    name: "3 Series",
+    chassis: "E30",
+    year: "1980",
+    image: "bmwimages/3seires/1.jpg",
+  },
+];
 
-const bmw1: Car = {
-  id: "BMW 3 Series E46",
-  name: "3 Series",
-  chassis: "E46",
-  year: "2004",
-  image: "bmwimages/3series/1.jpg"
-}
-
-const bmw2: Car = {
-  id: "BMW 3 Series E30",
-  name: "3 Series",
-  chassis: "E30",
-  year: "1980",
-  image: "bmwimages/3series/2.jpg"
-} 
-interface Question{
+interface Question {
   question: string;
   car: Car;
 }
 
 const model: Question = {
   question: "What model is this?",
-  car: bmw1 // TODO: add a way to correlate this to whatever the randomly selected car is
-}
+  car: renderedCars[0].name, // TODO: add a way to correlate this to whatever the randomly selected car is
+};
 
 const chassis: Question = {
   question: "What chassis code does this car have?",
-  car: bmw1 // TODO: same as last instance
-}
+  car: renderedCars[0].chassis, // TODO: same as last instance
+};
 
 let currentCar: Car | null = null;
 
 let i: number = 0;
-function changeQuestion(): void{ // i would want to start with an array of "question object"
+function changeQuestion(): void {
+  // i would want to start with an array of "question object"
   // the first index of the array would be the basic guess the model question
   // next up would be chassis code and so on so forth
   //
-  // so current task is to figure out how to assign question attributes 
+  // so current task is to figure out how to assign question attributes
   // to an object
   const inputBox = document.getElementById("model-input") as HTMLInputElement;
-  let prompts: string[] = ['Guess the model...', 'Guess the Chassis Code...', 'Guess the Year...', 'Guess the Designer...'];
-  if(inputBox && (prompts[i] != null)){
+  let prompts: string[] = [
+    "Guess the model...",
+    "Guess the Chassis Code...",
+    "Guess the Year...",
+    "Guess the Designer...",
+  ];
+  if (inputBox && prompts[i] != null) {
     inputBox.placeholder = prompts[i];
     i++;
   }
   const model: Question = {
     question: "What model is this?",
-    car: bmw1 // TODO: add a way to correlate this to whatever the randomly selected car is
-  }
+    car: bmw1, // TODO: add a way to correlate this to whatever the randomly selected car is
+  };
 
   /*
   let questionsArr: Question[] = {model, chassis};
   return questionsArr[0];
   */
 }
-/*
-function loadNewImage(): Car{
-  let carArr: Car[] = [bmw1, bmw2];
-  const random = Math.floor(Math.random() * carArr.length);
-  const imgElement = document.getElementById('random-bmw') as HTMLImageElement;
-  const modelName = document.getElementById('model-input') as HTMLInputElement;
-  const chassisName = document.getElementById('chassis-code') as HTMLInputElement;
-  const modelSource = carArr[random].name;
-  const chassisSource = carArr[random].chassis;
-  const imageSource = carArr[random].image;
-  if(imgElement){
-    imgElement.src = imageSource;
-    modelName.src = modelSource;
-    chassisName.src = chassisSource;
-  }
-  return carArr[random];
-}
-*/
-function loadNewImage(): void{
+function loadNewImage(): void {
   let carArr: Car[] = [bmw1, bmw2];
   const random = Math.floor(Math.random() * carArr.length);
   currentCar = carArr[random];
-  const imgElement = document.getElementById('random-bmw') as HTMLImageElement;
-  if(imgElement){
+  const imgElement = document.getElementById("random-bmw") as HTMLImageElement;
+  if (imgElement) {
     imgElement.src = currentCar.image;
     imgElement.alt = "Guess the BMW!";
   }
 }
-window.addEventListener('DOMContentLoaded', loadNewImage);
+window.addEventListener("DOMContentLoaded", loadNewImage);
 
 function checkGuess(): void {
-  if(!currentCar){
+  if (!currentCar) {
     return;
   }
   const firstInput = document.getElementById("guess-input") as HTMLInputElement;
