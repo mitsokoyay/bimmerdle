b3de1d79a727c583a35688079d6df09e3ce15316
Dylan Berutti <beruttid@gmail.com>
2025-10-06 16:04:45 -0400

    removed the nonfunctional json

---
+++ cars.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.renderedCars = void 0;
+exports.renderedCars = [
+    {
+        id: "BMW 3 Series",
+        name: "3 Series",
+        chassis: "E46",
+        year: "2004",
+        image: "bmwimages/3series/1.jpg",
+    },
+];
